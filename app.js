abdaa7115338b843f9327f46212ef3bb68a9e017
meticcio1972 <a.catarinozzi@gmail.com>
2026-07-09 17:22:31 +0200

    Update and rename js to app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+=====================================
+SCADENZE SMART GDO ENTERPRISE
+Versione 3.0
+=====================================
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("🚀 Scadenze Smart GDO Enterprise avviato");
+
+});
