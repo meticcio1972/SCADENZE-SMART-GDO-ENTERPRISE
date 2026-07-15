cda684a72719084a4b1578179f90c83a5813f678
meticcio1972 <a.catarinozzi@gmail.com>
2026-07-16 00:14:18 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,9 @@
 const SUPABASE_URL = "https://vxmmdrjlblvxobyxhnmq.supabase.co";
 const SUPABASE_KEY = "sb_publishable_GmNQvQmE-xrVAqpvB-BFdA_O-rBDKOq";
 
-const supabase = window.supabase.createClient(
+const client = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
 
-window.supabaseClient = supabase;
+window.supabaseClient = client;
