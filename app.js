2efa3d92528d375d367e172cc901e3bc46f1e82f
meticcio1972 <a.catarinozzi@gmail.com>
2026-08-01 23:04:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -152,10 +152,7 @@ const prodotto = {
     pezzi_offerta: parseInt(document.getElementById("pezzi_offerta").value) || 0
 };
 
-    offerta: document.getElementById("offerta").checked,
-    pezzi_offerta: Number(document.getElementById("pezzi_offerta").value || 0)
-};
-
+   
     if (window.prodottoInModifica !== undefined) {
 
     const { data, error } = await window.supabaseClient
