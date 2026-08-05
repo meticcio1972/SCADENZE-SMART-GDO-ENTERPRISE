e0330dfa1f568744acc6adedf4b92803c8bb1d7f
meticcio1972 <a.catarinozzi@gmail.com>
2026-08-05 21:23:35 +0200

    Update model.js

--- model.js
+++ model.js
@@ -1,1 +1,13 @@
+"use strict";
 
+const Modal = {
+
+    apri() {
+        document.getElementById("productModal").style.display = "flex";
+    },
+
+    chiudi() {
+        document.getElementById("productModal").style.display = "none";
+    }
+
+};
